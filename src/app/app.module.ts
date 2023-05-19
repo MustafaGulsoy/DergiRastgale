8bf8f3ea18e329734867a9eafaea1d53bcac1937
Mustafa Gülsoy <mustafa30045@gmail.com>
2023-05-19 10:24:16 +0300

    0.1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import {MatMenuModule} from '@angular/material/menu';
+import { ContentComponent } from './component/content/content.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { TurnJSComponent} from './component/turn-js/turn-js.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ContentComponent,
+    FooterComponent,
+    TurnJSComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
